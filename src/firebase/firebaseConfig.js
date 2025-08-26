8402bde6ba857a18e0ea04f32d82cc49437f1b41
JJathusan <info.jekilanjathusan@gmail.com>
2025-08-25 22:58:20 -0700

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,13 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0BlNPTbOfHCIuzeIlcYqJewu_pr6ZCZo",
-  authDomain: "donation-system-a7eec.firebaseapp.com",
-  projectId: "donation-system-a7eec",
-  storageBucket: "donation-system-a7eec.firebasestorage.app",
-  messagingSenderId: "745255217966",
-  appId: "1:745255217966:web:40827fd5f9ec77e6fee8bf",
-  measurementId: "G-GC6Q9B1PYP"
+  apiKey: "AIzaSyXXXX",
+  authDomain: "myapp.firebaseapp.com",
+  projectId: "myapp",
+  storageBucket: "myapp.appspot.com",
+  messagingSenderId: "123456",
+  appId: "1:123456:web:abcdef"
 };
 
 // Initialize Firebase
@@ -18,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 export default app;
+
